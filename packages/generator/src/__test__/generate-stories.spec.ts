e46dd358b5186d4836337e1b6d34840239a0b9e2
Naimul Haque <naimulcsx@gmail.com>
2025-03-09 16:43:48 +0800

    fix(generator): fix button story file

---
+++ packages/generator/src/__test__/generate-stories.spec.ts
@@ -0,0 +1,96 @@
+import { describe, it, expect } from 'vitest';
+import { generateComponents } from '../core/generate-components.js';
+import path from 'path';
+import fs from 'fs';
+const normalize = (str: string) => {
+  return str.replace(/\n/g, '').replace(/\s+/g, ' ');
+};
+
+const expected = [
+  `
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "../index";
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: ["primary"],
+      description: "Button variant style",
+    },
+    size: {
+      control: { type: "select" },
+      options: ["sm"],
+      description: "Button size",
+    },
+    children: {
+      control: "text",
+      description: "Button content",
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// Primary Button story
+export const Primary: Story = {
+  args: {
+    variant: "primary",
+    size: "sm",
+    
+    children: "Primary Button",
+  },
+};
+
+// SizeSm Button story
+export const SizeSm: Story = {
+  args: {
+    variant: "primary",
+    size: "sm",
+    
+    children: "Size Sm Button",
+  },
+};
+
+// Disabled Button story
+export const Disabled: Story = {
+  args: {
+    variant: "primary",
+    size: "sm",
+    disabled: true,
+    children: "Disabled Button",
+  },
+};
+`,
+];
+
+describe('generate-stories', () => {
+  it('should generate story for the button component', () => {
+    const content = `
+    /**
+     * @component Button
+     * @props fullWidth | loading | leftSection | rightSection
+     *
+     * @target root @size sm [ h-[42px] ]
+     * @target root @variant primary [ bg-red-500 hover:bg-red-900 focus:outline-none focus-within:ring-2 focus-within:ring-yellow-500 text-xl ]
+     */
+    `;
+    const result = generateComponents(content);
+
+    fs.writeFileSync(
+      path.join(__dirname, 'test.txt'),
+      result.stories[0].fileContent
+    );
+
+    expect(normalize(result.stories[0].fileContent)).toBe(
+      normalize(expected[0])
+    );
+  });
+});
